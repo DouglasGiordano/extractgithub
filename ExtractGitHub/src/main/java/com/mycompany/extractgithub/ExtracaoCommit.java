a63abb01786a3431bcc35546c50a6d6a928a8e84
Douglas Montanha Giordano <douglasmontanhagiordano@hotmail.com>
2017-03-17 11:21:56 -0300

    Projeto inicial.

---
+++ ExtractGitHub/src/main/java/com/mycompany/extractgithub/ExtracaoCommit.java
@@ -0,0 +1,41 @@
+/*
+ */
+package com.mycompany.extractgithub;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.eclipse.egit.github.core.Issue;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.RepositoryId;
+import org.eclipse.egit.github.core.service.IssueService;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
+/**
+ *
+ * @author Douglas Montanha Giordano
+ */
+public class ExtracaoCommit {
+    public static void main(String[] args) {
+        try {
+            RepositoryService service = new RepositoryService();
+            RepositoryId repo = new RepositoryId("rails", "rails");
+            Repository repositorio = service.getRepository(repo);
+            IssueService serviceIssue = new IssueService();
+            HashMap<String, String> parametros = new HashMap<String, String>();
+            parametros.put("01-01-2011", "03-25-2017");
+            List<Issue> issues = serviceIssue.getIssues(repositorio,
+                    parametros);
+            int i=0;
+            for(Issue issue: issues){
+                System.out.println(issue.getTitle()+" - "+issue.getCreatedAt());
+                i++;
+            }
+            System.out.println(i);
+        } catch (IOException ex) {
+            Logger.getLogger(ExtracaoIssue.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
